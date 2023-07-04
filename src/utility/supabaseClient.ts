a39d9354cbb4a957b9674c4851e12ccca6dd4b97
Mateus Abelli <mateusabelli@gmail.com>
2023-07-03 22:29:02 -0300

    Swap supabase client lib to refine lib

--- src/utility/supabaseClient.ts
+++ src/utility/supabaseClient.ts
@@ -1,4 +1,4 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@refinedev/supabase";
 
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY as string;
