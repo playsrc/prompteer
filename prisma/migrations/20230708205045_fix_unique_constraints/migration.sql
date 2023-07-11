c8c46a126049ae5e070fec9844c4ac253127c35c
Mateus Abelli <mateusabelli@gmail.com>
2023-07-10 21:53:59 -0300

    fix unique constraints

---
+++ prisma/migrations/20230708205045_fix_unique_constraints/migration.sql
@@ -0,0 +1,14 @@
+-- DropIndex
+DROP INDEX "comments_prompt_id_key";
+
+-- DropIndex
+DROP INDEX "comments_user_id_key";
+
+-- DropIndex
+DROP INDEX "prompts_ai_model_id_key";
+
+-- DropIndex
+DROP INDEX "prompts_language_id_key";
+
+-- DropIndex
+DROP INDEX "prompts_user_id_key";
