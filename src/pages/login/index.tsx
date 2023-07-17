770121f1410c2df2df2b307d13febc3dc3d01095
Mateus Abelli <mateusabelli@gmail.com>
2023-07-16 21:07:18 -0300

    Update login styling

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,29 @@
+import { Article, GitHub } from "@mui/icons-material";
+import { Stack, Typography } from "@mui/material";
 import { AuthPage } from "@refinedev/mui";
 
 import { GetServerSideProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import Link from "next/link";
+import { ReactNode } from "react";
 
 export default function Login() {
   return (
     <AuthPage
       type="login"
+      rememberMe={false}
+      forgotPasswordLink={false}
+      registerLink={false}
+      title={
+        <Link href="/" style={{ color: "unset", textDecoration: "none" }}>
+          <Stack direction="row" spacing={1} alignItems="center">
+            <Article fontSize="large" color="primary" />
+            <Typography fontSize="24px" fontWeight="500">
+              Prompteer
+            </Typography>
+          </Stack>
+        </Link>
+      }
       providers={[
         {
           name: "github",
@@ -17,6 +34,14 @@ export default function Login() {
           label: "with Discord",
         },
       ]}
+      renderContent={(content: ReactNode, title: ReactNode) => (
+        <>
+          {title}
+          {content}
+          <Typography>Please sign in with GitHub or Discord</Typography>
+          <Typography>Email sign in is not working at the moment.</Typography>
+        </>
+      )}
     />
   );
 }
